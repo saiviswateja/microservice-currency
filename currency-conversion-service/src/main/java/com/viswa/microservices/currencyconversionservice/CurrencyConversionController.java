2be38061d50620b26f3e7d0b2fcc55c6a98e4485
saiviswatejasreerama <saiviswatejasreerama@outlook.com>
2021-12-12 22:01:30 +0530

    added basic services wit feign

---
+++ currency-conversion-service/src/main/java/com/viswa/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,47 @@
+package com.viswa.microservices.currencyconversionservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+            ) {
+        CurrencyConversionBean currencyConversionBean = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversionBean(currencyConversionBean.getId(),currencyConversionBean.getFrom(), currencyConversionBean.getTo()
+                , currencyConversionBean.getConversionMultiple(), quantity,quantity.multiply(currencyConversionBean.getConversionMultiple()),currencyConversionBean.getPort());
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(
+            @PathVariable("from") String from,
+            @PathVariable("to") String to,
+            @PathVariable("quantity") BigDecimal quantity
+    ) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class,
+                uriVariables);
+        CurrencyConversionBean currencyConversionBean = responseEntity.getBody();
+        return new CurrencyConversionBean(currencyConversionBean.getId(),currencyConversionBean.getFrom(), currencyConversionBean.getTo()
+                , currencyConversionBean.getConversionMultiple(), quantity,quantity.multiply(currencyConversionBean.getConversionMultiple()),currencyConversionBean.getPort());
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/viswa/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.viswa.currencyexchangeservice;
+
+import org.omg.CORBA.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
